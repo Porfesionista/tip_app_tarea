004f0b15eee884afd3ee022423a44dd52625cc32
Cesar_Garcia <111899276+Porfesionista@users.noreply.github.com>
2024-01-17 10:36:09 -0700

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Proj05_Tip_Calculator_Cesar_Garcia"
+include(":app")
